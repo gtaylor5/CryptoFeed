c77c230416171389db85b9ce6d80db1c75bf503a
gtaylor5 <taylor.gerard13@gmail.com>
2018-02-08 21:02:35 -0500

    Better portfolio change dialogs

--- app/src/main/java/com/cryptoinc/cryptofeed/PortfolioFragment.java
+++ app/src/main/java/com/cryptoinc/cryptofeed/PortfolioFragment.java
@@ -3,19 +3,27 @@ package com.cryptoinc.cryptofeed;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
@@ -36,6 +44,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import Utilities.CurrencyInfo;
 import Utilities.CurrencyInfoPortfolioAdapter;
@@ -53,13 +62,19 @@ public class PortfolioFragment extends Fragment {
     PieChart pieChart;
     LinearLayoutManager manager;
 
+    NestedScrollView scrollView;
+
     FirebaseUser currentUser;
     DatabaseReference databaseReference;
 
+
+    String[] currencyList;
     HashMap<String, Double> portfolio = new HashMap<>(); // (BTC, 23.0000)
 
     private DecimalFormat defaultformatter = new DecimalFormat("$#,###.00");
     private DecimalFormat percentFormatter = new DecimalFormat("#,###.00 %");
+    private DecimalFormat percentFormatterLessThanZero = new DecimalFormat("0.000 %");
+    private DecimalFormat priceFormatterLessThanZero = new DecimalFormat("$ 0.000");
     private double value;
 
     public PortfolioFragment() {}
@@ -76,6 +91,7 @@ public class PortfolioFragment extends Fragment {
         portfolioValue = view.findViewById(R.id.portfolio_value);
         portfolioDollarChange = view.findViewById(R.id.portfolioDollarChange);
         portfolioPercentageChange = view.findViewById(R.id.portfolioPercentageChange);
+        scrollView = view.findViewById(R.id.scrollView);
 
         pieChart = view.findViewById(R.id.portfolioChart);
 
@@ -101,9 +117,11 @@ public class PortfolioFragment extends Fragment {
                 databaseReference.child(adapter.currencies.get(viewHolder.getAdapterPosition()).getSymbol()).setValue(null);
             }
         });
-
         itemTouchHelper.attachToRecyclerView(portfolioList);
 
+        if(getActivity() != null) {
+            currencyList = ((MainActivity) getActivity()).currencySymbolMap.keySet().toArray(new String[0]);
+        }
         manager = new LinearLayoutManager(getActivity());
         manager.setOrientation(LinearLayout.VERTICAL);
         portfolioList.setLayoutManager(manager);
@@ -112,9 +130,11 @@ public class PortfolioFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(currentUser != null && getActivity() != null) {
+                    showDialog();
+                    /*
                     new MaterialDialog.Builder(getActivity())
                             .title("Choose a Currency")
-                            .items(((MainActivity) getActivity()).currencySymbolMap.keySet())
+                            .customView(R.layout.auto_complete, false)
                             .itemsCallbackSingleChoice(-1, new MaterialDialog.ListCallbackSingleChoice() {
                                 @Override
                                 public boolean onSelection(MaterialDialog dialog, View itemView, int which, final CharSequence text) {
@@ -125,11 +145,7 @@ public class PortfolioFragment extends Fragment {
                                             .input("1.2345", null, false, new MaterialDialog.InputCallback() {
                                                 @Override
                                                 public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
-                                                    try {
-                                                        databaseReference.child(text.toString()).setValue(Double.parseDouble(input.toString()));
-                                                    } catch (Exception e){
-                                                        Toast.makeText(getActivity(),"Error adding to your portfolio. Please try again.", Toast.LENGTH_LONG).show();
-                                                    }
+
                                                     dialog.dismiss();
                                                 }
                                             }).show();
@@ -137,11 +153,51 @@ public class PortfolioFragment extends Fragment {
                                     return true;
                                 }
                             }).show();
+                            */
                 }
             }
         });
 
         return view;
+    }
+
+    void showDialog() {
+        if(getActivity() != null) {
+
+            MaterialDialog materialDialog = new MaterialDialog.Builder(getActivity())
+                    .title("Add To Portfolio")
+                    .backgroundColor(getResources().getColor(R.color.background, null))
+                    .titleColor(getResources().getColor(R.color.white, null))
+                    .customView(R.layout.auto_complete, true)
+                    .positiveText("Add").build();
+            View view = materialDialog.getCustomView();
+            if (view != null ) {
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_dropdown_item_1line, currencyList);
+                final AutoCompleteTextView autoCompleteTextView = view.findViewById(R.id.autoCompleteTextView);
+                autoCompleteTextView.setThreshold(1);
+                autoCompleteTextView.setAdapter(adapter);
+                final EditText quantity = view.findViewById(R.id.quantity);
+
+                materialDialog.getBuilder().onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        try {
+                            if(quantity.getText().length() != 0 && ((MainActivity)getActivity()).currencySymbolMap.keySet().contains(autoCompleteTextView.getText().toString())) {
+                                databaseReference.child(autoCompleteTextView.getText().toString()).setValue(Double.parseDouble(quantity.getText().toString()));
+                            } else if(quantity.getText().length() == 0){
+                                Toast.makeText(getActivity(),"Please enter a valid quantity.", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getActivity(),"Invalid Symbol. Please try again.", Toast.LENGTH_LONG).show();
+                            }
+                        } catch (Exception e){
+                            Toast.makeText(getActivity(),"Error adding to your portfolio. Please try again.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+                materialDialog.show();
+            }
+        }
     }
 
     public void notifyDataSetChanged() {
@@ -179,11 +235,16 @@ public class PortfolioFragment extends Fragment {
                 }
             }
         }
-        percentageChange = ((value /sumPrevDay)-1)*100;
-        portfolioValue.setText(defaultformatter.format(value));
-        portfolioDollarChange.setText(defaultformatter.format((value -sumPrevDay)));
+        percentageChange = ((value /sumPrevDay)-1);
+        Log.d("PERCENTAGECHANGE", "calculatePortfolioValue: " + percentageChange);
+        if(value < 1){
+            portfolioValue.setText(priceFormatterLessThanZero.format(value));
+        } else {
+            portfolioValue.setText(defaultformatter.format(value));
+        }
+        portfolioDollarChange.setText((value -sumPrevDay) > 1 ? defaultformatter.format((value -sumPrevDay)) : priceFormatterLessThanZero.format((value -sumPrevDay)));
         portfolioDollarChange.setTextColor((value -sumPrevDay) < 0 ? getResources().getColor(R.color.negative_red, null) : getResources().getColor(R.color.positive, null));
-        portfolioPercentageChange.setText(percentFormatter.format(percentageChange));
+        portfolioPercentageChange.setText(Math.abs(percentageChange) > 1 ? percentFormatter.format(percentageChange) : percentFormatterLessThanZero.format(percentageChange));
         portfolioPercentageChange.setTextColor((value -sumPrevDay) < 0 ? getResources().getColor(R.color.negative_red, null) : getResources().getColor(R.color.positive, null));
     }
 
@@ -259,7 +320,9 @@ public class PortfolioFragment extends Fragment {
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
                     if(snapshot.getValue() != null) {
                         try {
-                            portfolio.put(snapshot.getKey(), Double.parseDouble(snapshot.getValue().toString()));
+                            if(dataSnapshot.getValue() != null) {
+                                portfolio.put(dataSnapshot.getKey(), Double.valueOf(dataSnapshot.getValue().toString()));
+                            }
                         } catch (Exception e){
                             if(getActivity() != null) {
                                 FirebaseAnalytics.getInstance(getActivity()).logEvent("Error_converting_to_double", new Bundle());
@@ -279,7 +342,9 @@ public class PortfolioFragment extends Fragment {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 try {
-                    portfolio.put(dataSnapshot.getKey(), (Double) dataSnapshot.getValue());
+                    if(dataSnapshot.getValue() != null) {
+                        portfolio.put(dataSnapshot.getKey(), Double.valueOf(dataSnapshot.getValue().toString()));
+                    }
                     notifyDataSetChanged();
                 } catch (Exception e){
                     if(getActivity() != null) {
@@ -292,7 +357,9 @@ public class PortfolioFragment extends Fragment {
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
                 if(dataSnapshot.getValue() != null && dataSnapshot.getKey() != null) {
                     try {
-                        portfolio.put(dataSnapshot.getKey(), (Double)dataSnapshot.getValue());
+                        if(dataSnapshot.getValue() != null) {
+                            portfolio.put(dataSnapshot.getKey(), Double.valueOf(dataSnapshot.getValue().toString()));
+                        }
                         notifyDataSetChanged();
                     } catch (Exception e){
                         if(getActivity() != null) {
